f97e980e196f93a9363384c27360a34ebe325216
ItsGibbs <thepapeyeku@gmail.com>
2022-05-09 20:04:09 +0200

    add pieces

--- Checkers/checkers/board.py
+++ Checkers/checkers/board.py
@@ -3,7 +3,7 @@ from .constants import BLACK, ROWS, RED, SQUARE_SIZE
 
 class Board:
     def __init__(self):
-        self.board = [[]]
+        self.board = []
         self.selected_piece = None
         self.red_left = self.white_left = 12
         self.red_kings = self.white_kings = 0
@@ -14,3 +14,5 @@ class Board:
             for col in range(row % 2, ROWS, 2):
                 pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
+    def create_board(self):
+        pass 

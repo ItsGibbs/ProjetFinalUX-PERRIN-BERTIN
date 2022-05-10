5ae67de345e18fd962c1ad01e6537eb97824a29a
ItsGibbs <thepapeyeku@gmail.com>
2022-05-10 15:51:01 +0200

    add queens + assets

--- Checkers/checkers/piece.py
+++ Checkers/checkers/piece.py
@@ -1,4 +1,4 @@
-from .constants import RED, WHITE, SQUARE_SIZE, GREY
+from .constants import RED, WHITE, SQUARE_SIZE, GREY, CROWN
 import pygame
 
 class Piece:
@@ -31,6 +31,8 @@ class Piece:
         radius = SQUARE_SIZE//2 - self.PADDING
         pygame.draw.circle(win, self.color, (self.x, self.y), radius + self.OUTLINE)
         pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+        if self.king:
+            win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2))
 
     def __repr__(self):
         return str(self.color)

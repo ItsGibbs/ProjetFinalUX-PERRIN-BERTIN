31a8c5d2975931c98b7dcdeb9c29155adcc9e870
ItsGibbs <thepapeyeku@gmail.com>
2022-05-13 17:01:31 +0200

    get moves/simulate moves

--- Checkers/minimax/algorithm.py
+++ Checkers/minimax/algorithm.py
@@ -4,4 +4,34 @@ import pygame
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 
-def minimax(position, depth, max_player, game)
+def minimax(position, depth, max_player, game):
+    if depth == 0 or position.winner() != None:
+        return position.evaluate(), position
+
+    if max_player:
+        maxEval = float('-inf')
+        best_move = None
+        for move in get_all_moves(position, WHITE, game):
+            pass
+
+def simulate_move(piece, move, board, game, skip):
+    board.move(piece, move[0], move[1])
+    if skip:
+        board.remove(skip)
+
+    return board
+
+
+
+def get_all_moves(board, color, game):
+    moves = []
+
+    for piece in board.get_all_pieces(color):
+        valid_moves = board.get_valid_moves(piece)
+        for move, skip in valid_moves.items():
+            temp_board = deepcopy(board)
+            temp_piece = temp_board.get_piece(piece.row, piece.col)
+            new_board = simulate_move(piece, move, temp_board, skip)
+            moves.append([new_board, piece])
+
+    return moves
